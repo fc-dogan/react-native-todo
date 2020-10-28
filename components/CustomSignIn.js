f2868d694534a9953a4188f9449d9868b2f54ab7
Fatma <fceyda@gmail.com>
2020-10-27 18:00:36 -0700

    refactor app.js create todo component

---
+++ components/CustomSignIn.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Authenticator, SignIn } from 'aws-amplify-react-native';
+
+class MyCustomSignUp extends React.Component {
+  constructor() {
+    super();
+    this.gotoSignIn = this.gotoSignIn.bind(this);
+  }
+
+  gotoSignIn() {
+    // to switch the authState to 'signIn'
+    this.props.onStateChange('signIn',{});
+  }
+
+  render() {
+    return (
+      <div>
+        {/* only render this component when the authState is 'signUp' */}
+        { this.props.authState === 'signUp' && 
+        <div>
+          My Custom SignUp Component
+          <button onClick={this.gotoSignIn}>Goto SignIn</button>
+        </div>
+        }
+      </div>
+    );
+  }
+}
