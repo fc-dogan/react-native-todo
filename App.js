ce1db13afa109e17886f13071586d3cf7178d69e
Fatma <fceyda@gmail.com>
2020-10-27 18:11:44 -0700

    add custom signup config

--- App.js
+++ App.js
@@ -4,24 +4,50 @@ import {
 } from 'react-native'
 import ToDo from './components/ToDo'
 import Amplify, { Auth  } from 'aws-amplify'
-import { Authenticator, SignIn } from 'aws-amplify-react-native'
+import { withAuthenticator } from 'aws-amplify-react-native'
 
 import config from './aws-exports'
 Amplify.configure(config)
 
+const signUpConfig = {
+  header: 'My Customized Sign Up',
+  hideAllDefaults: true,
+  defaultCountryCode: '1',
+  signUpFields: [
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 1,
+      type: 'string'
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 1,
+      type: 'string'
+    },
+  ] 
+};
+
+
 const App = () => {
- 
+  
   return (
     <View style={styles.container}>
 
       <ToDo />
     </View>
     
-  )
-}
-
-const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', padding: 20 },
-})
-
-export default App;
+    )
+  }
+  
+  const styles = StyleSheet.create({
+    container: { flex: 1, justifyContent: 'center', padding: 20 },
+  })
+  
+  
+  
+  // export default withAuthenticator(App);
+  export default withAuthenticator(App, { signUpConfig });
